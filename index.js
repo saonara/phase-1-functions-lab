3a97b25416a86f45ba52e44bb79eb90dae23e7fd
odongo <saonarawestinc@outlook.com>
2024-03-22 00:57:03 +0300

    Completed functions lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const HQ = 42;
+
+function distanceFromHqInBlocks(start) {
+  return Math.abs(HQ - start);
+}
+
+function distanceFromHqInFeet(start) {
+  return distanceFromHqInBlocks(start) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  const totalDistance = distanceTravelledInFeet(start, destination);
+
+  if (totalDistance <= 400) {
+    return 0;
+  } else if (totalDistance > 2500) {
+    return "cannot travel that far";
+  } else if (totalDistance > 400 && totalDistance <= 2000) {
+    return (totalDistance - 400) * 0.02;
+  } else if (totalDistance > 2000) {
+    return 25;
+  }
+}
